197832294505b2240a6d8c2722a5cc9f187df718
Milan Koniar <milankoniar@gmail.com>
2019-04-16 19:01:20 +0200

    initial init .. created joke component

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { JokeComponent } from './joke/joke.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    JokeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
